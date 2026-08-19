81c7265552a3e4d691a851f6f7dcc5980aba9608
dirodrigueza34 <dianaisabelrodrigueza@gmail.com>
2026-08-18 23:43:28 -0500

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,17 +1,22 @@
 <?php
-$host = "127.0.0.1";
-$puerto = 3306;
-$usuario = "root";
-$pasword = "";
-$base_datos = "sistem_invent";
+// Módulo 4: Comunicación y API - Conexión de red segura hacia la nube
+$host = "://aivencloud.com";
+$puerto = 28084;
+$usuario = "avnadmin";
+$base_datos = "defaultdb";
 
+// Lee de forma invisible la contraseña desde el panel de entorno del servidor Render
+$pasword = getenv('DB_PASSWORD');
+
+// Inicialización de la API orientada a objetos mysqli
 $conexion = new mysqli($host, $usuario, $pasword, $base_datos, $puerto);
 
 if ($conexion->connect_error) {
-    die("Error de conexión: " . $conexion->connect_error);
+    die("Error de comunicación HTTP: " . $conexion->connect_error);
 }
 
 $conexion->set_charset("utf8");
 ?>
+
 
 
