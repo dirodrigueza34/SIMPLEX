9596adfec8590ea0e2ccdf382db5dcf775372d2c
dirodrigueza34 <dianaisabelrodrigueza@gmail.com>
2026-08-23 11:09:39 -0500

    fix: colocar host relacional correcto de suparbase

--- conexion.php
+++ conexion.php
@@ -1,6 +1,6 @@
 <?php
 // Módulo 4: Comunicación y API - Puente de datos real con Supabase (Cali)
-$host = "://supabase.com"; // Servidor AWS de alta velocidad en la nube
+$host = "aws-0-us-west-2.pooler.supabase.com"; 
 $puerto = 5432; // Puerto transaccional estándar de datos relacionales
 $usuario = "postgres.epqcdgehzkmcopumcczh"; // Tu usuario oficial asignado por Supabase
 $base_datos = "postgres"; // Base de datos maestra del proyecto
@@ -11,12 +11,12 @@ $pasword = getenv('DB_PASSWORD');
 // Inicialización de la API orientada a objetos mysqli
 $conexion = new mysqli($host, $usuario, $pasword, $base_datos, $puerto);
 
-// Validación perimetral del canal de comunicación HTTP
+
 if ($conexion->connect_error) {
     die("Error de comunicación real con la nube: " . $conexion->connect_error);
 }
 
-// Configuración del set de caracteres universal para evitar distorsiones en las tablas horizontales
+
 $conexion->set_charset("utf8");
 ?>
 
