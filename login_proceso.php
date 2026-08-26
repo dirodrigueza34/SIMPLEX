2d23097ab99872faba064555de2796a089eb7906
dirodrigueza34 <dianaisabelrodrigueza@gmail.com>
2026-08-25 23:45:06 -0500

    fix: incorporar credenciales multiusuario en el proceso de login

--- login_proceso.php
+++ login_proceso.php
@@ -4,11 +4,11 @@ include "conexion.php";
 $user = isset($_POST['usuario']) ? trim($_POST['usuario']) : '';
 $pass = isset($_POST['contrasena']) ? trim($_POST['contrasena']) : '';
 
-if (($user === "MAGVD12" && $pass === "cali123") || ($user === "Andres01" && $pass === "simplex2026")) {
-    echo "<script>window.location.href='productos.php';</script>";
+if (($user === "MAGVD12" && $pass === "cali123") || ($user === "INSTRUCTOR" && $pass === "sena2026")) {
+    header("Location: productos.php");
     exit();
 } else {
-    echo "<script>window.location.href='index.php?error=invalido';</script>";
+    header("Location: index.php?error=invalido");
     exit();
 }
 ?>
