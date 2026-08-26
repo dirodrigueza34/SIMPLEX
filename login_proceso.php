c3467e356bc3e1cd500406bc2bc824e60980ca71
dirodrigueza34 <dianaisabelrodrigueza@gmail.com>
2026-08-25 23:40:30 -0500

    fix: incorporar credenciales multiusuario en el proceso de login

--- login_proceso.php
+++ login_proceso.php
@@ -1,18 +1,18 @@
 <?php
-// Se capturan los datos enviados de forma segura
+include "conexion.php";
+
 $user = isset($_POST['usuario']) ? trim($_POST['usuario']) : '';
 $pass = isset($_POST['contrasena']) ? trim($_POST['contrasena']) : '';
 
-// Credenciales base de prueba según tu captura (Usuario: MAGVD12)
-if ($user === "MAGVD12" && $pass === "cali123") {
-    // Si es correcto, redirige al menú general de la aplicación (productos.php)
+if (($user === "MAGVD12" && $pass === "cali123") || ($user === "Andres01" && $pass === "simplex2026")) {
     header("Location: productos.php");
     exit();
 } else {
-    // Si falla, devuelve a la pantalla inicial informando el error de validación
+    
     header("Location: index.php?error=invalido");
     exit();
 }
 ?>
+
 
 
